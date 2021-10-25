d7160fb6291b29a8f7b73a4ffe4b5c553b4892c3
TB831 <druu831@gmail.com>
2021-10-25 14:30:38 -0700

    inputs and headers styles

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,19 +14,23 @@ const Login = () => {
     <div className='card-login'>
       <h4>Welcome!</h4>
       <h5>Sign into your account!</h5>
-      <label htmlFor='email'>Email</label>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        id='email'
-        type='email'
-      />
-      <label htmlFor='password'>Password</label>
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        id='password'
-        type='password'
-      />
-      <a href='#'>Recover Password</a>
+      <div className='card-inputs'>
+        <label htmlFor='email'>Email</label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          id='email'
+          type='email'
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          id='password'
+          type='password'
+        />
+      </div>
+      <a href='#' className='card-link'>
+        Recover Password
+      </a>
       <button onClick={() => onSubmitSignIn()} className='button'>
         Sign In
       </button>
